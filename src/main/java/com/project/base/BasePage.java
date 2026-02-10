aec1a8176d1885acba65383e0404c1cb73fa545b
Kousar <2461890@cognizant.com>
2026-02-10 12:41:30 +0530

    removed

--- src/main/java/com/project/base/BasePage.java
+++ src/main/java/com/project/base/BasePage.java
@@ -9,7 +9,7 @@ public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected JavascriptExecutor js;
-    int a=0;
+    int a=9;
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
